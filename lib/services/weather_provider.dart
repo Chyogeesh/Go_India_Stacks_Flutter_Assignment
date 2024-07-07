7ad8022d2e33cfa45f1f91ca4da27f37f6e71ef3
Chyogeesh <108213999+Chyogeesh@users.noreply.github.com>
2024-07-07 12:44:49 +0530

    Create weather_provider.dart

---
+++ lib/services/weather_provider.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:weather_app/models/weather.dart';
+import 'package:weather_app/services/weather_service.dart';
+
+class WeatherProvider with ChangeNotifier {
+  Weather? _weather;
+  bool _isLoading = false;
+  String? _errorMessage;
+
+  Weather? get weather => _weather;
+  bool get isLoading => _isLoading;
+  String? get errorMessage => _errorMessage;
+
+  Future<void> fetchWeather(String city) async {
+    _isLoading = true;
+    _errorMessage = null;
+    notifyListeners();
+
+    try {
+      final data = await WeatherService().fetchWeather(city);
+      _weather = Weather.fromJson(data);
+    } catch (error) {
+      _errorMessage = error.toString();
+    }
+
+    _isLoading = false;
+    notifyListeners();
+  }
+}
